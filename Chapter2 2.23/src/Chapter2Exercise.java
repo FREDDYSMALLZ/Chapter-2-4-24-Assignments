9b1a498202b8a25ddee6433bad5a3eaead0904e7
Fredrick Ondieki <freddysmallz@gmail.com>
2017-02-05 23:27:50 -0600

    Second commint after editing the code
    Signed-off-by: Fredrick Ondieki <freddysmallz@gmail.com>

--- Chapter2 2.23/src/Chapter2Exercise.java
+++ Chapter2 2.23/src/Chapter2Exercise.java
@@ -1,8 +1,23 @@
-
+import java.util.Scanner;
 public class Chapter2Exercise {
 
 	public static void main(String[] args) {
 // Calculating the cost of driving
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print(" Enter the driving distance:");
+		double distance = input.nextDouble();
+		
+		System.out.print("Enter miles per gallon:");
+		  double fuel = input.nextDouble();
+		  
+		  System.out.print("Enter price per gallon:");
+		  double pricepergal = input.nextDouble();
+		  
+		  double cost = distance / 25.5 * pricepergal;
+		  
+		  System.out.print("The cost of driving is $" + cost);
+		 
 	}
 
 }
